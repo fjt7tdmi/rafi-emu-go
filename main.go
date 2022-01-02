bf00cfff8de35cb6d551af7f719acb4741421f57
Akifumi Fujita <fujita.akifumi@gmail.com>
2022-01-02 15:16:28 +0900

    implement LUI, UnknownOp and decode()

--- main.go
+++ main.go
@@ -5,6 +5,44 @@ import (
 	"io/ioutil"
 )
 
+// op interface
+type Op interface {
+	Execute()
+}
+
+// unknown op
+type UnknownOp struct{}
+
+func (op UnknownOp) Execute() {}
+
+func (op UnknownOp) String() string {
+	return "unknown"
+}
+
+// LUI
+type LUI struct {
+	rd  uint32
+	imm uint32
+}
+
+func (op LUI) Execute() {}
+
+func (op LUI) String() string {
+	return "LUI"
+}
+
+// decode
+func decode(insn uint32) Op {
+	funct7 := insn & 0x7f
+
+	if funct7 == 0b0110111 {
+		rd := (insn >> 7) & 0x1f
+		return LUI{rd: rd}
+	}
+
+	return UnknownOp{}
+}
+
 func main() {
 	bytes, err := ioutil.ReadFile("./rafi-prebuilt-binary/riscv-tests/isa/rv32ui-p-add.bin")
 	if err != nil {
@@ -16,6 +54,7 @@ func main() {
 			uint32(bytes[i+2])<<16 |
 			uint32(bytes[i+1])<<8 |
 			uint32(bytes[i])
-		fmt.Printf("%08x\n", insn)
+		op := decode(insn)
+		fmt.Printf("%08x %s\n", insn, op)
 	}
 }
